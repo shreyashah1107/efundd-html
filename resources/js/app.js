15bf4f77e9fc09ff14e2f9a0eea0d805bd6a335e
Shreya Shah <shreya@darwindigital.com>
2024-08-26 11:31:34 +0530

    Hero Slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,29 @@
+// Hero Card Slider
+var swiper = new Swiper(".hero--swiper", {
+  slidesPerView: 1.01,
+  spaceBetween: 20,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+
+  breakpoints: {
+    576: {
+      slidesPerView: 1.8,
+    },
+    992: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 2.75,
+    },
+    1400: {
+      slidesPerView: 3.1,
+    },
+  },
+});
+
 // Account Management Card Slider
 var swiper = new Swiper(".account-management--swiper", {
   slidesPerView: 1.01,
